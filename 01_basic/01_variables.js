2f165ab9d606caa58455b76378f0a4b5ba16791e
pankaj katre <111595958+panku722@users.noreply.github.com>
2023-08-11 10:35:57 +0000

    conversion of data type

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+// const name ="pankaj";
+// let age ="24";
+// let email ="pk@gmail.com";
+// const address ="bhopal";
+// console.table([name,age,email,address]);
+
+// let names ="panku"
+// let year =2023
+// let isLoogedIn = false
+
+let score = "33a"
+// console.log(typeof score);
+let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber);
+
+let someNumber = "33";
+let sameNumber = String(someNumber);
+console.log(typeof sameNumber);
